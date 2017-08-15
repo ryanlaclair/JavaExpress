b026517c231c6b03f00264d1ff6cfc6e2400e1c8
Ryan LaClair <ryanlaclair@gmail.com>
2017-08-14 20:36:25 -0400

    Added javadoc comments to the test simulation

--- src/test/java/TestJavaExpress.java
+++ src/test/java/TestJavaExpress.java
@@ -5,19 +5,33 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
+ * This is a class to test the JavaExpress framework. It creates a server
+ * that simulates a school with Students. Server routes are defined in the
+ * main method.
+ *
  * @author Ryan LaClair - rlaclair@bu.edu
  */
 public class TestJavaExpress {
 
+    /**
+     * The program entry point. Create a JavaExpress server, set up routes,
+     * and start the server listening on port 8999.
+     *
+     * @param args the command line arguments
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
         Map<String, Student> studentList = new HashMap<>();
 
+        // create the JavaExpress application
         JavaExpress app = new JavaExpress();
 
+        // welcome the client to school
         app.get("/", (req, res) -> {
             res.send("Welcome to school");
         });
 
+        // get the list of students
         app.get("/students", (req, res) -> {
             if (studentList.isEmpty()) {
                 res.status(HTTPStatus.NOT_FOUND);
@@ -34,6 +48,7 @@ public class TestJavaExpress {
             }
         });
 
+        // add a student to the list
         app.post("/students", (req, res) -> {
             String studentName = req.body();
             studentList.put(studentName, new Student(studentName));
@@ -41,6 +56,7 @@ public class TestJavaExpress {
             res.send("Added student " + studentName);
         });
 
+        // get the list of classes for a student
         app.get("/students/:name/classes", (req, res) -> {
             String name = req.params().get(("name"));
             if (studentList.containsKey(name)) {
@@ -52,6 +68,7 @@ public class TestJavaExpress {
             }
         });
 
+        // add a class to a student
         app.post("/students/:name/classes", (req, res) -> {
            String name = req.params().get("name");
            if (studentList.containsKey(name)) {
@@ -62,6 +79,7 @@ public class TestJavaExpress {
            }
         });
 
+        // start the server listening on port 8999
         app.listen(8999);
     }
 
